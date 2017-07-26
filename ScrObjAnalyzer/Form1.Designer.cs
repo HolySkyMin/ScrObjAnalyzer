67834d2775b845ec0c555ff6b69ac4c17a64f2e1
thiEFcat <fsda109238@gmail.com>
2017-07-26 23:31:44 +0900

    First but unfinished.

--- ScrObjAnalyzer/Form1.Designer.cs
+++ ScrObjAnalyzer/Form1.Designer.cs
@@ -28,12 +28,234 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ModePanel = new System.Windows.Forms.GroupBox();
+            this.FilterBtn = new System.Windows.Forms.Button();
+            this.Mix2P = new System.Windows.Forms.RadioButton();
+            this.Mix2 = new System.Windows.Forms.RadioButton();
+            this.Mix4 = new System.Windows.Forms.RadioButton();
+            this.MixM = new System.Windows.Forms.RadioButton();
+            this.Mix6 = new System.Windows.Forms.RadioButton();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.TextList = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Track = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.StartPos = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.EndPos = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Speed = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.EndType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.OpenDialog = new System.Windows.Forms.OpenFileDialog();
+            this.ModePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ModePanel
+            // 
+            this.ModePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ModePanel.Controls.Add(this.FilterBtn);
+            this.ModePanel.Controls.Add(this.Mix2P);
+            this.ModePanel.Controls.Add(this.Mix2);
+            this.ModePanel.Controls.Add(this.Mix4);
+            this.ModePanel.Controls.Add(this.MixM);
+            this.ModePanel.Controls.Add(this.Mix6);
+            this.ModePanel.Location = new System.Drawing.Point(645, 64);
+            this.ModePanel.Name = "ModePanel";
+            this.ModePanel.Size = new System.Drawing.Size(234, 182);
+            this.ModePanel.TabIndex = 1;
+            this.ModePanel.TabStop = false;
+            this.ModePanel.Text = "Game Mode";
+            // 
+            // FilterBtn
+            // 
+            this.FilterBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FilterBtn.Location = new System.Drawing.Point(6, 130);
+            this.FilterBtn.Name = "FilterBtn";
+            this.FilterBtn.Size = new System.Drawing.Size(222, 46);
+            this.FilterBtn.TabIndex = 4;
+            this.FilterBtn.Text = "Reload";
+            this.FilterBtn.UseVisualStyleBackColor = true;
+            this.FilterBtn.Click += new System.EventHandler(this.FilterBtn_Click);
+            // 
+            // Mix2P
+            // 
+            this.Mix2P.AutoSize = true;
+            this.Mix2P.Location = new System.Drawing.Point(6, 42);
+            this.Mix2P.Name = "Mix2P";
+            this.Mix2P.Size = new System.Drawing.Size(92, 16);
+            this.Mix2P.TabIndex = 3;
+            this.Mix2P.TabStop = true;
+            this.Mix2P.Text = "2Mix+ [3, 4]";
+            this.Mix2P.UseVisualStyleBackColor = true;
+            // 
+            // Mix2
+            // 
+            this.Mix2.AutoSize = true;
+            this.Mix2.Location = new System.Drawing.Point(6, 20);
+            this.Mix2.Name = "Mix2";
+            this.Mix2.Size = new System.Drawing.Size(86, 16);
+            this.Mix2.TabIndex = 2;
+            this.Mix2.TabStop = true;
+            this.Mix2.Text = "2Mix [1, 2]";
+            this.Mix2.UseVisualStyleBackColor = true;
+            // 
+            // Mix4
+            // 
+            this.Mix4.AutoSize = true;
+            this.Mix4.Location = new System.Drawing.Point(6, 64);
+            this.Mix4.Name = "Mix4";
+            this.Mix4.Size = new System.Drawing.Size(132, 16);
+            this.Mix4.TabIndex = 2;
+            this.Mix4.TabStop = true;
+            this.Mix4.Text = "4Mix [9, 10, 11, 12]";
+            this.Mix4.UseVisualStyleBackColor = true;
+            // 
+            // MixM
+            // 
+            this.MixM.AutoSize = true;
+            this.MixM.Location = new System.Drawing.Point(6, 108);
+            this.MixM.Name = "MixM";
+            this.MixM.Size = new System.Drawing.Size(213, 16);
+            this.MixM.TabIndex = 1;
+            this.MixM.TabStop = true;
+            this.MixM.Text = "Million Mix [31, 32, 33, 34, 35, 36]";
+            this.MixM.UseVisualStyleBackColor = true;
+            // 
+            // Mix6
+            // 
+            this.Mix6.AutoSize = true;
+            this.Mix6.Location = new System.Drawing.Point(6, 86);
+            this.Mix6.Name = "Mix6";
+            this.Mix6.Size = new System.Drawing.Size(178, 16);
+            this.Mix6.TabIndex = 0;
+            this.Mix6.TabStop = true;
+            this.Mix6.Text = "6Mix [25, 26, 27, 28, 29, 30]";
+            this.Mix6.UseVisualStyleBackColor = true;
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadBtn.Location = new System.Drawing.Point(645, 12);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(234, 46);
+            this.LoadBtn.TabIndex = 5;
+            this.LoadBtn.Text = "Load file";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // TextList
+            // 
+            this.TextList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.Track,
+            this.Time,
+            this.Type,
+            this.StartPos,
+            this.EndPos,
+            this.Speed,
+            this.EndType});
+            this.TextList.GridLines = true;
+            this.TextList.Location = new System.Drawing.Point(12, 12);
+            this.TextList.MultiSelect = false;
+            this.TextList.Name = "TextList";
+            this.TextList.Size = new System.Drawing.Size(627, 233);
+            this.TextList.TabIndex = 6;
+            this.TextList.UseCompatibleStateImageBehavior = false;
+            this.TextList.View = System.Windows.Forms.View.Details;
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            this.ID.Width = 40;
+            // 
+            // Track
+            // 
+            this.Track.Text = "Track";
+            this.Track.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Track.Width = 50;
+            // 
+            // Time
+            // 
+            this.Time.Text = "Time";
+            this.Time.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Time.Width = 150;
+            // 
+            // Type
+            // 
+            this.Type.Text = "Type";
+            this.Type.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Type.Width = 70;
+            // 
+            // StartPos
+            // 
+            this.StartPos.Text = "StartPos";
+            this.StartPos.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.StartPos.Width = 70;
+            // 
+            // EndPos
+            // 
+            this.EndPos.Text = "EndPos";
+            this.EndPos.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.EndPos.Width = 70;
+            // 
+            // Speed
+            // 
+            this.Speed.Text = "Speed";
+            this.Speed.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Speed.Width = 70;
+            // 
+            // EndType
+            // 
+            this.EndType.Text = "EndType";
+            this.EndType.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.EndType.Width = 70;
+            // 
+            // OpenDialog
+            // 
+            this.OpenDialog.DefaultExt = "txt";
+            this.OpenDialog.FileName = "*";
+            this.OpenDialog.Filter = "Text file (*.txt)|*.txt|All file|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(891, 257);
+            this.Controls.Add(this.TextList);
+            this.Controls.Add(this.LoadBtn);
+            this.Controls.Add(this.ModePanel);
+            this.MinimumSize = new System.Drawing.Size(907, 296);
+            this.Name = "Form1";
+            this.Text = "ScrObjAnalyzer 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ModePanel.ResumeLayout(false);
+            this.ModePanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.GroupBox ModePanel;
+        private System.Windows.Forms.RadioButton Mix2P;
+        private System.Windows.Forms.RadioButton Mix2;
+        private System.Windows.Forms.RadioButton Mix4;
+        private System.Windows.Forms.RadioButton MixM;
+        private System.Windows.Forms.RadioButton Mix6;
+        private System.Windows.Forms.Button FilterBtn;
+        private System.Windows.Forms.Button LoadBtn;
+        private System.Windows.Forms.ListView TextList;
+        private System.Windows.Forms.ColumnHeader Track;
+        private System.Windows.Forms.ColumnHeader Time;
+        private System.Windows.Forms.ColumnHeader Type;
+        private System.Windows.Forms.ColumnHeader StartPos;
+        private System.Windows.Forms.ColumnHeader EndPos;
+        private System.Windows.Forms.ColumnHeader Speed;
+        private System.Windows.Forms.ColumnHeader EndType;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.OpenFileDialog OpenDialog;
     }
 }
 
--- ScrObjAnalyzer/Form1.cs
+++ ScrObjAnalyzer/Form1.cs
@@ -7,14 +7,164 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ScrObjAnalyzer
 {
     public partial class Form1 : Form
     {
+        public string OriginText;
+        public List<ListData> ListDatas = new List<ListData>();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult res = OpenDialog.ShowDialog();
+            if(res.Equals(DialogResult.OK))
+            {
+                Text = OriginText += (" - " + OpenDialog.SafeFileName);
+                StreamReader reader = new StreamReader(OpenDialog.OpenFile());
+                List<string> strList = new List<string>();
+                while(reader.Peek() != -1)
+                {
+                    strList.Add(reader.ReadLine());
+                }
+                ReadFumenFile(strList.ToArray());
+            }
+        }
+
+        private void ReadFumenFile(string[] target)
+        {
+            char[] parseTool = new char[] { ' ' };
+            Dictionary<int, int> trackDic = new Dictionary<int, int>();
+            int totalCount = -1;
+
+            for (int i = 0; i < target.Length; i++)
+            {
+                string[] data = target[i].Split(parseTool, StringSplitOptions.RemoveEmptyEntries);
+                if (data.Length > 0 && data[0].Equals("\t\tEventNoteData") && i > 9)
+                {
+                    ListDatas.Add(new ListData());
+                    totalCount++;
+                }
+                else if (data.Length > 2 && data[1].Equals("track"))
+                {
+                    int val = int.Parse(data[3]);
+                    ListDatas[totalCount].Track = val;
+
+                    int mixVal = val;
+                    if (val >= 1 && val <= 2) { mixVal = 100; }
+                    else if (val >= 3 && val <= 4) { mixVal = 101; }
+                    else if (val >= 9 && val <= 12) { mixVal = 110; }
+                    else if (val >= 25 && val <= 30) { mixVal = 120; }
+                    else if (val >= 31 && val <= 36) { mixVal = 121; }
+                    if (!trackDic.ContainsKey(mixVal)) { trackDic.Add(mixVal, 0); }
+                    trackDic[mixVal]++;
+                    ListDatas[totalCount].ID = trackDic[mixVal];
+                }
+                else if (data.Length > 2 && data[1].Equals("absTime"))
+                {
+                    double val = double.Parse(data[3]);
+                    ListDatas[totalCount].Time = val;
+                }
+                else if (data.Length > 2 && data[1].Equals("type"))
+                {
+                    int val = int.Parse(data[3]);
+                    ListDatas[totalCount].Type = val;
+                }
+                else if (data.Length > 2 && data[1].Equals("startPosx"))
+                {
+                    double val = double.Parse(data[3]);
+                    ListDatas[totalCount].StartPos = val;
+                }
+                else if (data.Length > 2 && data[1].Equals("endPosx"))
+                {
+                    double val = double.Parse(data[3]);
+                    ListDatas[totalCount].EndPos = val;
+                }
+                else if (data.Length > 2 && data[1].Equals("speed"))
+                {
+                    double val = double.Parse(data[3]);
+                    ListDatas[totalCount].Speed = val;
+                }
+                else if (data.Length > 2 && data[1].Equals("endType"))
+                {
+                    int val = int.Parse(data[3]);
+                    ListDatas[totalCount].EndType = val;
+                }
+            }
+
+            AddToList(ListDatas);
+        }
+
+        private void FilterBtn_Click(object sender, EventArgs e)
+        {
+            List<ListData> NewData = new List<ListData>();
+
+            for(int i = 0; i < ListDatas.Count; i++)
+            {
+                if(Mix2.Checked)
+                {
+                    if(ListDatas[i].Track >= 1 && ListDatas[i].Track <= 2) { NewData.Add(ListDatas[i]); }
+                }
+                else if(Mix2P.Checked)
+                {
+                    if (ListDatas[i].Track >= 3 && ListDatas[i].Track <= 4) { NewData.Add(ListDatas[i]); }
+                }
+                else if(Mix4.Checked)
+                {
+                    if (ListDatas[i].Track >= 9 && ListDatas[i].Track <= 12) { NewData.Add(ListDatas[i]); }
+                }
+                else if(Mix6.Checked)
+                {
+                    if (ListDatas[i].Track >= 25 && ListDatas[i].Track <= 30) { NewData.Add(ListDatas[i]); }
+                }
+                else if(MixM.Checked)
+                {
+                    if (ListDatas[i].Track >= 31 && ListDatas[i].Track <= 36) { NewData.Add(ListDatas[i]); }
+                }
+            }
+
+            AddToList(NewData);
+        }
+
+        private void AddToList(List<ListData> datas)
+        {
+            TextList.Items.Clear();
+            for (int i = 0; i < datas.Count; i++)
+            {
+                ListViewItem item = new ListViewItem(datas[i].ID.ToString());
+                item.SubItems.Add(datas[i].Track.ToString());
+                item.SubItems.Add(datas[i].Time.ToString());
+                item.SubItems.Add(datas[i].Type.ToString());
+                item.SubItems.Add(datas[i].StartPos.ToString());
+                item.SubItems.Add(datas[i].EndPos.ToString());
+                item.SubItems.Add(datas[i].Speed.ToString());
+                item.SubItems.Add(datas[i].EndType.ToString());
+
+                TextList.Items.Add(item);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OriginText = Text;
+        }
+    }
+
+    public class ListData
+    {
+        public int ID { get; set; }
+        public int Track { get; set; }
+        public int Type { get; set; }
+        public double Time { get; set; }
+        public double StartPos { get; set; }
+        public double EndPos { get; set; }
+        public double Speed { get; set; }
+        public int EndType { get; set; }
     }
 }
